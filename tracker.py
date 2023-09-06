10051525e459a155c9876d4976e74bca2b592561
Curt Odar <curoda@users.noreply.github.com>
2023-09-06 11:20:37 -0400

    Update tracker.py
    replacing toggles and performance text with a table.

--- tracker.py
+++ tracker.py
@@ -51,28 +51,33 @@ def main():
             index_data = (index_data.pct_change() + 1).cumprod() - 1  # Convert to cumulative return
             ax.plot(index_data.index, index_data, label=name, linestyle='--')
 
+        # Create a table to store the relative performance
+        performance_table = pd.DataFrame(index=indexes.keys())
+
         for score, group in grouped_stocks:
-            if st.checkbox(f"Show performance for stocks with score {score}?"):
-                avg_stock_data = pd.DataFrame()
-                for index, row in group.iterrows():
-                    stock_start_date = row['Purchase Date']
-                    stock_end_date = row['Sell Date'] if not pd.isnull(row['Sell Date']) else end_date
-                    data = fetch_data(row['Symbol'], stock_start_date, stock_end_date)
-                    data = (data.pct_change() + 1).cumprod() - 1  # Convert to cumulative return
-                    avg_stock_data[row['Symbol']] = data
-                    ax.plot(data.index, data, label=row['Symbol'])  # Plot individual stock
+            avg_stock_data = pd.DataFrame()
+            for index, row in group.iterrows():
+                stock_start_date = row['Purchase Date']
+                stock_end_date = row['Sell Date'] if not pd.isnull(row['Sell Date']) else end_date
+                data = fetch_data(row['Symbol'], stock_start_date, stock_end_date)
+                data = (data.pct_change() + 1).cumprod() - 1  # Convert to cumulative return
+                avg_stock_data[row['Symbol']] = data
 
-                # Calculate average percent change for the group of stocks
-                avg_stock_data['Average'] = avg_stock_data.mean(axis=1)
+            # Calculate average percent change for the group of stocks
+            avg_stock_data['Average'] = avg_stock_data.mean(axis=1)
 
-                # Calculate and display the difference between the group's average performance and each index
-                differences = []
-                for name, ticker in indexes.items():
-                    index_data = fetch_data(ticker, avg_stock_data.index.min(), avg_stock_data.index.max())
-                    index_data = (index_data.pct_change() + 1).cumprod() - 1  # Convert to cumulative return
-                    difference = (avg_stock_data['Average'].iloc[-1] - index_data.iloc[-1]) * 100
-                    differences.append(f"{difference:.2f}% more than the {name}")
-                st.write(f"Score {score} stocks gained {' , '.join(differences)}.")
+            # Calculate the difference between the group's average performance and each index
+            differences = {}
+            for name, ticker in indexes.items():
+                index_data = fetch_data(ticker, avg_stock_data.index.min(), avg_stock_data.index.max())
+                index_data = (index_data.pct_change() + 1).cumprod() - 1  # Convert to cumulative return
+                difference = (avg_stock_data['Average'].iloc[-1] - index_data.iloc[-1]) * 100
+                differences[name] = difference
+
+            performance_table[score] = differences.values()
+
+        # Display the performance table with color-coded values
+        st.table(performance_table.style.applymap(lambda x: 'color: green' if x > 0 else 'color: red'))
 
         ax.set_title("Stock Performance vs Indexes")
         ax.set_xlabel("Date")
